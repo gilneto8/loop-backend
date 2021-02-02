3c1275f1075ded7aa6195ee0ceb87234cf7e4489
Gil Neto <gilneto8@gmail.com>
2021-02-02 12:34:26 +0000

    configured authn routes by default.

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,20 +1,20 @@
 import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
 import { User } from './users/users.service';
-import { LocalAuthGuard } from './auth/strategies/local/local.guard';
 import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/strategies/jwt/jwt.guard';
+import { Public } from './auth/decorators/public-route';
+import { LocalAuthGuard } from './auth/strategies/local/local.guard';
 
 @Controller()
 export class AppController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req: { user: User }) {
     return this.authService.login(req.user);
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get('user')
   getUser(@Request() req: { user: User }) {
     return req.user;
--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -6,6 +6,8 @@ import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 import { JwtStrategy } from './strategies/jwt/jwt.strategy';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './strategies/jwt/jwt.guard';
 
 // TODO USERS ONLY FOR TESTING PURPOSES
 
@@ -18,7 +20,15 @@ import { JwtStrategy } from './strategies/jwt/jwt.strategy';
       signOptions: { expiresIn: '60s' },
     }),
   ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
   exports: [AuthService],
 })
 export class AuthModule {}
