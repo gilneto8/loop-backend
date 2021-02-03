a710c490258c774bf1bde7f718d32f20959bccd2
Gil Neto <gilneto8@gmail.com>
2021-02-03 09:26:05 +0000

    created endpoints for get and create accounts

--- api/src/account/account.controller.ts
+++ api/src/account/account.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Request } from '@nestjs/common';
+import { AccountService } from './account.service';
+import { TCreate } from './account.types';
 
 @Controller('accounts')
-export class AccountController {}
+export class AccountController {
+  constructor(private accountService: AccountService) {}
+
+  @Get(':id')
+  getAccount(@Param('id') id: string) {
+    return this.accountService.get({ id: +id });
+  }
+
+  @Post()
+  createAccount(@Request() req: { body: TCreate }) {
+    return this.accountService.create(req.body);
+  }
+}
