7f15cbb88104c4cfab371b4e83b8ff52c9fcd006
Gil Neto <gilneto8@gmail.com>
2021-02-05 13:28:24 +0000

    added exception logger.

--- api/src/app.providers.ts
+++ api/src/app.providers.ts
@@ -1,9 +1,15 @@
-import { APP_GUARD } from '@nestjs/core';
+import 'source-map-support/register';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './internals/auth/strategies/jwt/jwt.guard';
+import { ExceptionLoggerFilter } from './internals/filters/exception-logger';
 
 export const GLOBAL_PROVIDERS = [
   {
     provide: APP_GUARD,
     useClass: JwtAuthGuard,
+  },
+  {
+    provide: APP_FILTER,
+    useClass: ExceptionLoggerFilter,
   },
 ];
---
+++ api/src/internals/filters/exception-logger.ts
@@ -0,0 +1,58 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import moment from 'moment';
+import { ErrorMessages } from '../../utils/enums/error-messages';
+import { clc } from '@nestjs/common/utils/cli-colors.util';
+
+@Catch()
+export class ExceptionLoggerFilter implements ExceptionFilter {
+  private logger = new Logger('Exception');
+
+  catch(exception: unknown, host: ArgumentsHost): void {
+    const ctx = host.switchToHttp();
+    const { url, method } = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+
+    const now = moment().format('DD/MMM/YYYY:HH:mm:ss ZZ');
+
+    const e =
+      exception instanceof HttpException
+        ? {
+            status: exception.getStatus(),
+            message: exception.getResponse(),
+          }
+        : {
+            status: HttpStatus.INTERNAL_SERVER_ERROR,
+            message: ErrorMessages.UNKNOWN,
+          };
+    const statusKey = HttpStatus[e.status];
+
+    if (typeof e.message === 'object') {
+      /*
+      if messageContent arrives here as object, then
+        certainly has field 'message'
+      */
+      // @ts-ignore
+      e.message = e.message.message;
+    }
+
+    const entry = clc.red(
+      `${e.status} ${statusKey} [${now}] ${method} ${url} "${e.message}"`,
+    );
+    this.logger.log(entry);
+
+    response.status(e.status).json({
+      statusCode: e.status,
+      timestamp: now,
+      path: url,
+      message: e.message,
+    });
+  }
+}
